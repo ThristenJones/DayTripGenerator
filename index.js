f42c028a00a945de53ccfe54b7ccfc9a0bbfef0c
ThristenJones <Thristenjones@yahoo.com>
2021-08-19 20:51:18 -0500

    I added a new resturaunt array to my js file

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 "use strict"
-console.log("Thristen")
+
+let places = ['japan','germany','alaska','france'];
+let restuarants = ['chilis','red lobster','pluckers','hooters']
+
+function numberFinder (array){
+let i = Math.floor(Math.random()*4)
+return array[i]
+}
+
+let choiceOfPlaces = numberFinder(places);{
+    console.log(choiceOfPlaces)
+}
+
+let choiceOfResturaunts = numberFinder(restuarants);{
+    console.log(choiceOfResturaunts)
+}
+
+let choiceOfTransportation = numberFinder(transportation);{
+    console.log(choiceOfTransportation)
+}
+
+let choiceofEntertainment = numberFinder(entertainment);{
+    console.log(choiceofEntertainment)
+}
+
+let usersAnswer = prompt("Are you satisfied with your planned out trip? Type Yes to confirm or No to reselect")
+    while(usersAnswer === "No"){
+        function numberFinder (array){
+            let i = Math.floor(Math.random()*4)
+            return array[i]
+            }
+
+            let choiceOfPlaces = numberFinder(places);{
+                console.log(choiceOfPlaces)
+            }
+            
+            let choiceOfResturaunts = numberFinder(restuarants);{
+                console.log(choiceOfResturaunts)
+            }
+            
+            let choiceOfTransportation = numberFinder(transportation);{
+                console.log(choiceOfTransportation)
+            }
+            
+            let choiceofEntertainment = numberFinder(entertainment);{
+                console.log(choiceofEntertainment)
+            }
+            usersAnswer = prompt("Are you satisfied with your planned out trip? Type Yes to confirm or No to reselect")
+            if(usersAnswer === "Yes"){
+                console.log("Congratulations! Your trip has been booked, Enjoy!")
+                
+            }
+
+            }
+            
+         
+            
