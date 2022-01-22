8f0f60fafee584ca18b54069ab1af0034f5bf291
ThristenJones <Thristenjones@yahoo.com>
2022-01-21 19:13:15 -0600

    added additional functionality to planning out a day trip generator

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict"
 
-let places = ['japan','germany','alaska','france'];
-let restuarants = ['chilis','red lobster','pluckers','hooters'];
+let places = ['japan','germany','hawaii','france'];
+let restuarants = ['chilis','red lobster','pluckers','texas roadhouse'];
 let transportation = ['corvette', 'mustang','lamborghini','ferrai'];
-let entertainment = ['tv','concert','comedy show','movies']
+let entertainment = ['museum','concert','comedy show','movies']
 
 function numberFinder (array){
 let i = Math.floor(Math.random()*4)
@@ -11,46 +11,54 @@ return array[i]
 }
 
 let choiceOfPlaces = numberFinder(places);{
-    console.log(choiceOfPlaces)
 }
 
 let choiceOfResturaunts = numberFinder(restuarants);{
-    console.log(choiceOfResturaunts)
+
 }
 
 let choiceOfTransportation = numberFinder(transportation);{
-    console.log(choiceOfTransportation)
 }
 
 let choiceofEntertainment = numberFinder(entertainment);{
-    console.log(choiceofEntertainment)
 }
 
-let usersAnswer = prompt("Are you satisfied with your planned out trip? Type Yes to confirm or No to reselect")
-    while(usersAnswer === "No"){
+function Alert() {
+    alert(`Hello there, thanks for choosing to have the best time of your life! We've set up a whole trip for you! You will be flying out to ${choiceOfPlaces.toUpperCase()} first class and enjoying a free meal at a fancy resturaunt called ${choiceOfResturaunts.toUpperCase()}. For your transportation, you will be arriving in a ${choiceOfTransportation.toUpperCase()} and your entertainment will be a ${choiceofEntertainment.toUpperCase()} night `)
+}
+ 
+Alert()
+let usersAnswer = prompt("Please let us know if your satisfied with your trip we planned for you! Type Yes to confirm your trip or No to randomly reselect a whole new one!")
+    if(usersAnswer.toUpperCase() === "YES") {
+        document.querySelector("body").innerHTML = `Congratulations! Your trip has been booked, Enjoy your stay in ${choiceOfPlaces.toUpperCase()} while eating at ${choiceOfResturaunts.toUpperCase()}. A ${choiceofEntertainment.toUpperCase()} night always seems pretty fun and let us know how fast the ${choiceOfTransportation.toUpperCase()} goes. All Web Developers check the console log for your confirmation number.!`
+                console.log("Congratulations. Your confirmation number is: 1122334")
+
+    }
+    while(usersAnswer.toUpperCase() === "NO"){
         function numberFinder (array){
             let i = Math.floor(Math.random()*4)
             return array[i]
             }
 
             let choiceOfPlaces = numberFinder(places);{
-                console.log(choiceOfPlaces)
             }
             
             let choiceOfResturaunts = numberFinder(restuarants);{
-                console.log(choiceOfResturaunts)
             }
             
             let choiceOfTransportation = numberFinder(transportation);{
-                console.log(choiceOfTransportation)
             }
             
             let choiceofEntertainment = numberFinder(entertainment);{
-                console.log(choiceofEntertainment)
             }
+            function reAlert() {
+                alert(`Didn't like that one, No worries! We've set up a whole new trip for you. So you will flying out to ${choiceOfPlaces.toUpperCase()} and enjoying a free meal at ${choiceOfResturaunts.toUpperCase()}. For transportation you will be arriving in a ${choiceOfTransportation.toUpperCase()} and your entertainment will be a ${choiceofEntertainment.toUpperCase()}`)
+            }
+            reAlert()
             usersAnswer = prompt("Are you satisfied with your planned out trip? Type Yes to confirm or No to reselect")
-            if(usersAnswer === "Yes"){
-                console.log("Congratulations! Your trip has been booked, Enjoy!")
+            if(usersAnswer.toUpperCase() === "YES"){
+                document.querySelector("body").innerHTML = `Congratulations! Your trip has been booked, Enjoy your stay in ${choiceOfPlaces.toUpperCase()} while eating at ${choiceOfResturaunts.toUpperCase()}. A ${choiceofEntertainment.toUpperCase()} night always seems pretty fun and let us know how fast the ${choiceOfTransportation.toUpperCase()} goes. All Web Developers check the console log for your confirmation number.!`
+                console.log("Congratulations. Your confirmation number is: 1122334")
                 
             }
 
